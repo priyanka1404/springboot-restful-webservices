22a8ccb190cb18464cac452968864e2193099ed9
priyanka1404 <priyankareddynomula111@gmail.com>
2024-03-30 13:47:41 -0500

    ModelMapper  libraray changes

--- src/main/java/com/example/springbootrestfulwebservices/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/com/example/springbootrestfulwebservices/SpringbootRestfulWebservicesApplication.java
@@ -1,10 +1,20 @@
 package com.example.springbootrestfulwebservices;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootRestfulWebservicesApplication {
+
+
+
+// we need to configure spring bean of model mapper
+@Bean  // it will register in application context
+ public ModelMapper modelMapper(){
+    return  new ModelMapper();
+ }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
--- src/main/java/com/example/springbootrestfulwebservices/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.example.springbootrestfulwebservices.dto.UserDto;
 import com.example.springbootrestfulwebservices.entity.User;
-import com.example.springbootrestfulwebservices.mapper.UserMapper;
+//import com.example.springbootrestfulwebservices.mapper.UserMapper;
 import com.example.springbootrestfulwebservices.repository.UserRepository;
 import com.example.springbootrestfulwebservices.service.UserService;
 
@@ -25,6 +26,7 @@ public class UserServiceImpl  implements UserService {
     * because if a spring bean(class) it has single paramaterized constructor we can omit autowired annotation
     */
     private  UserRepository userRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public User creatUser(User user) {  // rest api controller will call createUser method
@@ -74,78 +76,98 @@ public class UserServiceImpl  implements UserService {
     }
 
               /****************USER DTO*************/
+/**********REFACTORING THE  methods to use MODEL Mapper  class   ********/
 
 
-    @Override
-    public UserDto creatUser(UserDto userDto) {
-        /*  here the userDto is method argument
-        * we need to  store jpa entity in db
-        * so here we need to convert userdto to userjpa  entity  object
-        */
-        
-     // INSTEAD OF WRITING THIS LOGIC FOR EVERY MAPPING WE ARE GOING TO USE <MAPPER CLASS> WHERE IT HOLD CONVERSION LOGIC METHODS
-        //convert userDTO to JPA entity object
-         /* ----------- User user1 =new User(userDto.getId(),
-                                 userDto.getFirstName(),
-                                 userDto.getLastName(),
-                                 userDto.getEmail());  ----------------------*/
-//mapper  class method
-  User user = UserMapper.mapToUser(userDto);
+@Override
+public UserDto creatUser(UserDto userDto) {
+    /*  here the userDto is method argument
+    * we need to  store jpa entity in db
+    * so here we need to convert userdto to userjpa  entity  object
+    */
+    
+ // 1)INSTEAD OF WRITING THIS LOGIC FOR EVERY MAPPING WE ARE GOING TO USE <MAPPER CLASS> WHERE IT HOLD CONVERSION LOGIC METHODS
+    //convert userDTO to JPA entity object
+     /* ----------- User user1 =new User(userDto.getId(),
+                             userDto.getFirstName(),
+                             userDto.getLastName(),
+                             userDto.getEmail());  ----------------------*/
+//2)mapper  class method
+/*-- User user = UserMapper.mapToUser(userDto);--*/
+
+// 3) model mapper 
+User user = modelMapper.map(userDto,User.class);
+
 
 // we need to save user jpa entity  object into db by using save method
-                
-          /*--------User savedUser =  userRepository.save(user1);--------*/
-          User savedUser = userRepository.save(user);
-                // convert jpa entity into userDto
-                //restapi will expect saved user in response it contains primary key
-                /*---------------UserDto savedUserDto =  new UserDto(savedUser.getId(),
-                                                    savedUser.getFirstName(),
-                                                    savedUser.getLastName(),
-                                                    savedUser.getEmail());-----------------*/
+            
+      /*--------User savedUser =  userRepository.save(user1);--------*/
+      User savedUser = userRepository.save(user);
+            // convert jpa entity into userDto
+            // 1) restapi will expect saved user in response it contains primary key
+            /*---------------UserDto savedUserDto =  new UserDto(savedUser.getId(),
+                                                savedUser.getFirstName(),
+                                                savedUser.getLastName(),
+                                                savedUser.getEmail());-----------------*/
 
 
-         // mapper class method
-         UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
-         
-        return  savedUserDto;
-        
-    }
+     // 2)mapper class method
+     /***---UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);---***/
+     
+     
+     // 3) model mapper 
 
-    @Override
-    public UserDto getUserByIdDto(Long userId) {
+     UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
+     
+    return  savedUserDto;
+    
+}
 
-        Optional<User> optionalUser=userRepository.findById(userId);
-       
-        User user = optionalUser.get(); // it retrieves user object by id 
-    return UserMapper.mapToUserDto(user);
-        
-    }
+@Override
+public UserDto getUserByIdDto(Long userId) {
 
-    @Override
-    public List<UserDto> getAllUsersDto() {
-       List<User> users= userRepository.findAll();
+    Optional<User> optionalUser=userRepository.findById(userId);
+   
+    User user = optionalUser.get(); // it retrieves user object by id 
+/****------2) return UserMapper.mapToUserDto(user);-------*******/
+  return modelMapper.map(user, UserDto.class);
+    
+}
 
-        return  users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList()); 
-        // to convert  jpa to dto
-    }
+@Override
+public List<UserDto> getAllUsersDto() {
+   List<User> users= userRepository.findAll();
 
-    @Override
-    public UserDto updatUserDto(UserDto userDto) {//  user object as a method argument
+   /****------2) return  users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList()); ****/
 
-        //this user object contain all the updated information sent by the client 
-        
-        
-        User  existingUser= userRepository.findById(userDto.getId()).get();
-        // we got the exsisting user object from db and  
-        //we will update this existing user and will save back to db 
+   //3)model mapper
+   return  users.stream().map((user)->modelMapper.map(user,UserDto.class)).collect(Collectors.toList());
 
-        existingUser.setFirstName(userDto.getFirstName());
-        existingUser.setLastName(userDto.getLastName());
-        existingUser.setEmail(userDto.getEmail());
-        User updatedUser=userRepository.save(existingUser);
 
-        return  UserMapper.mapToUserDto(updatedUser);
-       
-    }
+    // to convert  jpa to dto
+}
+
+@Override
+public UserDto updatUserDto(UserDto userDto) {//  user object as a method argument
+
+    //this user object contain all the updated information sent by the client 
+    
+    
+    User  existingUser= userRepository.findById(userDto.getId()).get();
+    // we got the exsisting user object from db and  
+    //we will update this existing user and will save back to db 
+
+    existingUser.setFirstName(userDto.getFirstName());
+    existingUser.setLastName(userDto.getLastName());
+    existingUser.setEmail(userDto.getEmail());
+    User updatedUser=userRepository.save(existingUser);
+
+    /****--2)---return  UserMapper.mapToUserDto(updatedUser);-----*/
+
+    //3)modelmapper
+    return  modelMapper.map(updatedUser ,UserDto.class);
+   
+}
+
 
 } 
